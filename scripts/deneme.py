a1b6bc23c38eee9fbb9fdb08d7cb9cf411628ff4
BurakBulbul58 <bulbulb20@itu.edu.tr>
2023-07-02 14:29:24 +0300

    pozisyon kontrolu

--- scripts/deneme.py
+++ scripts/deneme.py
@@ -1,15 +1,14 @@
 import rospy
 from geometry_msgs.msg import TwistStamped,PoseStamped
 from ros_clients.msg import GeneralizedForce
-from math import sqrt,atan2
+import math
 
 class Tekne:
-    def __init__(self,istenen_x,istenen_y,istenen_aci):
+    def __init__(self,istenen_x,istenen_aci):
         rospy.init_node("burak",anonymous=True)
         rospy.Subscriber=("/nav/twist",TwistStamped,self.pose_callback)
         self.pub=rospy.Publisher("/force_control",GeneralizedForce,queue_size=10)
         self.x1=istenen_x
-        self.y1=istenen_y
         self.yaw1=istenen_aci
         self.integral_x=0
         self.integral_y=0
@@ -36,11 +35,9 @@ class Tekne:
     def goal(self):
         force_msg=GeneralizedForce()
         while not rospy.is_shutdown():
-            error_x=self.x1-self.x
-            error_y=self.y1-self.y
-            error_yaw=self.yaw1-self.yaw
-            force_x, self.lasterror_x, self.integral_x=self.pid_control(error_x, self.lasterror_x,self.integral_x)
-            force_y, self.lasterror_y, self.integral_y=self.pid_control(error_y, self.lasterror_y,self.integral_y)
+            distance=math.sqrt(((self.x1-self.x)**2)+((self.y1-self.y)**2))
+            error_yaw=math.atan2(self.y1-self.y,self.x1-self.x)
+            force_x, self.lasterror_x, self.integral_x=self.pid_control(distance, self.lasterror_x,self.integral_x)
             force_yaw, self.lasterror_yaw, self.integral_yaw=self.pid_control(error_yaw, self.lasterror_yaw,self.integral_yaw)
             force_msg.x=force_x
             #force_msg.y=force_y
